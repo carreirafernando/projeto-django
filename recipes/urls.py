1476fdc1e71514719cf1e79a362ec80f49861933
Fernando Alves Carreira <fe.alves_carreira@hotmail.com>
2024-11-20 18:26:55 -0300

    removendo arquivos desnecessarios

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from recipes.views import contato, home, my_view
+from recipes.views import home
 
 urlpatterns = [
     path('', home),
-    path('sobre/', my_view),
-    path('contato/', contato),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
     # return HttpResponse('HOME')
-    return render(request, 'recipes/home.html')
-
-def my_view(request):
-    # return HttpResponse('SOBRE')
-    return render(request, 'temp.html')
-
-def contato(request):
-    return HttpResponse('CONTATO')
+    return render(request, 'recipes/home.html'),
